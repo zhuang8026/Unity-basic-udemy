0d8c1246c50ecd2c6c4346ee8451e40ba269789e
Wiiliam_Chuang <william_chuang@asus.com>
2022-08-30 10:08:22 +0800

    ch2_3

--- C語法基礎/Ch2_3.cs
+++ C語法基礎/Ch2_3.cs
@@ -7,7 +7,27 @@ public class Ch2_3 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        int val_1 = 10;
+        int val_2 = 20;
+
+        bool val_3 = val_1 == val_2;
+        Debug.Log("bool == :" + val_3);
+
+        val_3 = val_1 != val_2;
+        Debug.Log("bool != :" + val_3);
+
+        val_3 = val_1 > val_2;
+        Debug.Log("bool > :" + val_3);
+
+        val_3 = val_1 < val_2;
+        Debug.Log("bool < :" + val_3);
+
+        val_3 = val_1 >= val_2;
+        Debug.Log("bool >= :" + val_3);
+
+        val_3 = val_1 <= val_2;
+        Debug.Log("bool <= :" + val_3);
+
     }
 
     // Update is called once per frame
